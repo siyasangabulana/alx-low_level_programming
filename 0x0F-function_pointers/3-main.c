55d1435adb5e0c6d480b2411a9bc1436f619e701
siyasangabulana <bulanasiyasanga@gmail.com>
2023-11-08 01:29:25 -0800

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+    int num1, num2, result;
+    int (*func)(int, int);
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+    func = get_op_func(argv[2]);
+
+    if (!func)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
+
+    result = func(num1, num2);
+    printf("%d\n", result);
+
+    return (0);
+}
